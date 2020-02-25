0c95f17a7a07561cd03b13f086692ca2623fa23a
Divine-Favour <divine-favour.o@ng.lopworks.com>
2020-02-25 06:14:17 +0100

    First commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,17 @@
+<template>
+    <div class="container">
+        <h3>Todos</h3>
+    </div>
+</template>
+
+<script>
+ import { mapGetters } from 'vuex'
+
+export default {
+    name: "Todos",
+}
+</script>
+
+<style>
+
+</style>3
